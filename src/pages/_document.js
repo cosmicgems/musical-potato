ba27571f04847a9b024f040d8c66e2b465849f96
Maliek Davis <maliekjdavis24@gmail.com>
2023-07-13 21:36:34 -0400

    meta add

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,8 +9,13 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Agdasima:wght@400;700&family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Bellota+Text:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Cantata+One&family=Cinzel:wght@400;500;600;700;800;900&family=Cormorant+Infant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Great+Vibes&family=Italiana&family=Martel+Sans:wght@200;300;400;600;700;800;900&family=Oswald:wght@200;300;400;500;600;700&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <meta property='og:image' content="/public/images/creditzen.png"/>
+        <meta property='og:title' content="My Credit Zen" />
+        <meta property='og:site_name' content="Credit Zen" />
+        <meta property='og:type' content="website" />
+
         </Head>
         <body>
           <Main />
